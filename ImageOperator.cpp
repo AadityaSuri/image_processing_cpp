111acc6a09f3a02198b761deb04005ac945c5a4c
Aaditya Suri <aadityasuri1@gmail.com>
2023-04-16 00:09:45 -0700

    benchmarking different versions of grayscale, no opencv implementation fastest

--- ImageOperator.cpp
+++ ImageOperator.cpp
@@ -50,3 +50,21 @@ void ImageOperator::to_gray_m3(const cv::Mat& input, cv::Mat& output) {
         }
     }
 }
+
+
+void ImageOperator::to_gray(const unsigned char* bgr_input,
+    const int width,
+    const int height,
+    const int channel,
+    unsigned char* gray_output) {
+    int index = 0;
+    int step = channel * width;
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width * channel; col += channel) {
+            gray_output[index] = 0.11 * bgr_input[row * step + col] +
+                0.59 * bgr_input[row * step + col + 1] +
+                0.3 * bgr_input[row * step + col + 2];
+            index++;
+        }
+    }
+}
--- ImageOperator.h
+++ ImageOperator.h
@@ -11,9 +11,9 @@ public:
     static void to_gray_m1(const cv::Mat& input, cv::Mat& output);
     static void to_gray_m2(const cv::Mat& input, cv::Mat& output);
     static void to_gray_m3(const cv::Mat& input, cv::Mat& output);
-    //static void to_gray(const unsigned char* input,
-    //    const int width,
-    //    const int height,
-    //    const int channel,
-    //    unsigned char* output);
+    static void to_gray(const unsigned char* input,
+        const int width,
+        const int height,
+        const int channel,
+        unsigned char* output);
 };
